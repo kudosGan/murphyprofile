ec51a94929849fed29212acfc67565a4a4a9adce
kudas g <kudosdesigner@hotmail.com>
2020-05-24 00:16:18 -0400

    Updates

---
+++ precache-manifest.776b67388e24537336b4b5677aa7c9cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71391851d13bf063931ed7e3e06c8610",
+    "url": "/murphyprofile/index.html"
+  },
+  {
+    "revision": "12ae020d1d7acef8fc58",
+    "url": "/murphyprofile/static/css/main.5bbb2cda.chunk.css"
+  },
+  {
+    "revision": "0a2287aa5b8167be7f6a",
+    "url": "/murphyprofile/static/js/2.7916c37e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/murphyprofile/static/js/2.7916c37e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ae020d1d7acef8fc58",
+    "url": "/murphyprofile/static/js/main.afe94b23.chunk.js"
+  },
+  {
+    "revision": "6d6306d6b77d484695d1",
+    "url": "/murphyprofile/static/js/runtime-main.e6c92d51.js"
+  }
+]);
